701082df2dddcee272f17409090ff6085044a201
Jim O'Donnell <jim@zooniverse.org>
2022-03-09 13:20:05 +0000

    Add playbill images with dates

---
+++ playbills/page.11ty.js
@@ -0,0 +1,43 @@
+function parseCanvasImage(image, config) {
+  const { resource } = image;
+  const { imageHeight, imageWidth } = config
+  const id = resource.service['@id'];
+  const src = `${id}/full/!${imageWidth},${imageHeight}/0/default.jpg`;
+  return src;
+}
+
+function parseCanvas(canvas, index, config) {
+  const locations = canvas.images.map(image => parseCanvasImage(image, config));
+  const metadata = {
+    "iiif:canvas": canvas['@id'],
+    priority: index + 1
+  };
+  return { locations, metadata };
+}
+
+module.exports = class Playbill {
+  data() {
+    return {
+      pagination: {
+        data: 'manifest.sequences[0].canvases',
+        size: 1
+      },
+      permalink: ({ pagination }) => `/playbills/${pagination.pageNumber}.html`
+    }
+  }
+
+  render({ config, manifest, dates, titles, pagination }) {
+    const [ canvas ] = pagination.items
+    const { width, height, images } = canvas
+    const { locations, metadata } = parseCanvas(canvas, pagination.pageNumber, config)
+    const [src] = locations
+    const canvasDates = dates.items.filter(item => item.target === canvas['@id'])
+    const stringDates = canvasDates.map(date => `<p>${date.body.value}</p>`)
+    return `
+    <svg width=700 style="float:left;" viewbox="0,0,1400,2000">
+      <image xlink:Href=${src} />
+    </svg>
+    ${stringDates.join('\n')}
+    `
+  }
+}
