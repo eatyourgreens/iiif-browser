3a78072199f960aa5a2bda31c9f41c07d44221d6
Jim O'Donnell <jim@zooniverse.org>
2022-03-09 14:37:22 +0000

    Add SVG rectangles

--- playbills/page.11ty.js
+++ playbills/page.11ty.js
@@ -15,6 +15,22 @@ function parseCanvas(canvas, index, config) {
   return { locations, metadata };
 }
 
+function parseTitleBox(annotation) {
+  const { body, target } = annotation
+  const [ url, fragment ] = target.split('#')
+  const [ key, values ] = fragment.split('=')
+  const [ x, y, width, height ] = values.split(',')
+  const text = body.value
+  return { x, y, width, height, text }
+}
+
+function scaledTitleBox(box, scale) {
+  const x = box.x / scale
+  const y = box.y / scale
+  const height = box.height / scale
+  const width = box.width / scale
+  return `<rect fill="transparent" stroke="orange" x=${x} y=${y} height=${height} width=${width} />`
+}
 module.exports = class Playbill {
   data() {
     return {
@@ -27,6 +43,7 @@ module.exports = class Playbill {
   }
 
   render({ config, manifest, dates, titles, pagination }) {
+    const { imageHeight, imageWidth } = config
     const [ canvas ] = pagination.items
     const { width, height, images } = canvas
     const { locations, metadata } = parseCanvas(canvas, pagination.pageNumber, config)
@@ -34,7 +51,10 @@ module.exports = class Playbill {
     const canvasDates = dates.items.filter(item => item.target === canvas['@id'])
     const stringDates = canvasDates.map(date => `<p>${date.body.value}</p>`)
     const canvasTitles = titles.items.filter(item => item.target.startsWith(canvas['@id']))
-    const stringTitles = canvasTitles.map(title => `<p>${title.body.value}</p>`)
+    const titleBoxes = canvasTitles.map(parseTitleBox)
+    const stringTitles = titleBoxes.map(box => `<p>${box.text}</p>`)
+    const scale = Math.max((canvas.height / imageHeight), (canvas.width / imageWidth))
+    const svgTitles = titleBoxes.map(box => scaledTitleBox(box, scale))
 
     const { href } = pagination
     const nextLink = href.next ? `<a href=..${href.next}>Next</a>` : 'Next'
@@ -47,6 +67,7 @@ module.exports = class Playbill {
     </nav>
     <svg width=700 style="float:left;" viewbox="0,0,1400,2000">
       <image xlink:Href=${src} />
+      ${svgTitles.join('\n')}
     </svg>
     ${stringDates.join('\n')}
     ${stringTitles.join('\n')}
